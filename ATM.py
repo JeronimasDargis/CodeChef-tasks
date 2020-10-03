40679d87ac1078bb765307668a4530c31a57ec07
Jeronimas Dargis <jeronimasd@gmail.com>
2020-10-03 23:55:34 +0100

    task: START01

--- ATM.py
+++ ATM.py
@@ -1,9 +1,8 @@
-x,y = map(float,input().split())
+x, y = map(float, input().split())
 if x % 5 == 0:
-    if x+.5<=y:
-        y-=(x+.5)
+    if x+.5 <= y:
+        y -= (x+.5)
 print(y)
-
 
 
 # x = 30
